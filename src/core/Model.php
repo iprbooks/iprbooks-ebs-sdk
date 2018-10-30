3b820248cdff3c47aa8a1e986a9976798b245798
andrey <andrey.cherkasov93@gmail.com>
2018-10-30 16:49:27 +0400

    v1

---
+++ src/core/Model.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Iprbooks\Ebs\Sdk\Core;
+
+use Exception;
+use Iprbooks\Ebs\Sdk\Client;
+
+abstract class Model extends Response
+{
+
+    /**
+     * Конструктор Model
+     * @param Client $client
+     * @param $response
+     * @throws Exception
+     */
+    public function __construct(Client $client, $response = null)
+    {
+        parent::__construct($client, $response);
+        return $this;
+    }
+
+
+    /**
+     * Получить метод api для вызова, опеределяется в потомках
+     */
+    abstract protected function getApiMethod();
+
+    /**
+     * Отправка запроса
+     * @param $id
+     */
+    public function get($id)
+    {
+        $apiMethod = $this->getApiMethod();
+
+        if ($id) {
+            $apiMethod = str_replace('{id}', $id, $apiMethod);
+        }
+
+        $this->response = $this->getClient()->makeRequest($apiMethod, array());
+        $this->data = $this->response['data'];
+    }
+
+}
